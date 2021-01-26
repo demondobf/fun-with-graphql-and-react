38d7f3963e867c3e37d69d57e595cb58deda19e4
demondo <demondocs1.6@gmail.com>
2021-01-26 22:57:21 +0100

    added global style

---
+++ src/globalStyle.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components/macro';
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --color-text: hsl(0, 0%, 0%);
+    --color-background: hsl(0, 0%, 100%);
+    --color-primary: hsl(210, 95%, 50%);
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    margin: 0;
+    padding: 2em;
+
+    color: var(--color-text);
+    background-color: var(--color-background);
+
+    font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+      'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  a {
+    color: var(--color-primary);
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+`;
+
+export default GlobalStyle;
